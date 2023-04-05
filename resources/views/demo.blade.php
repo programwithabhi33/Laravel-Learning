b840d68e9ad0ec90436538abbd88ff21358a849f
abhi <programwithabhi@gmail.com>
2023-04-05 22:40:17 +0530

    first laravel commit learn to route and serve view template files

---
+++ resources/views/demo.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <h3>Demo Page</h3>
+    </div>
+    <h3>Welcome to the demo page</h3>
+</body>
+</html>
